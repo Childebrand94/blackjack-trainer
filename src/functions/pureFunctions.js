c76133d51ef4ecbb241b63bed288781f620806cb
Chris Hildebrand <hildebrandc94@gmail.com>
2023-08-14 17:19:15 -0400

    Practice classNames and added TailWind

--- src/functions/pureFunctions.js
+++ src/functions/pureFunctions.js
@@ -1,9 +1,11 @@
-// hit:: -> Array -> Object
+// hit:: -> Array -> [Object, Array]
 const hit = (deckCards) => {
   const randomIndex = Math.floor(Math.random() * deckCards.length)
-  return deckCards[randomIndex]
+  const newDeck = [...deckCards.filter((_, index) => index !== randomIndex)]
+  return [deckCards[randomIndex], newDeck]
 }
 
+console.log('hit')
 console.log(hit(Array.from({ length: 100 }, (_, index) => ({ id: index + 1 }))))
 
 // handTotal:: -> Array -> Number
@@ -13,9 +15,36 @@ const handTotal = (hand) => {
   }, 0)
 }
 
+console.log()
+console.log('handTotal')
 console.log(
   handTotal([
     { value: 10, suit: 'hearts' },
     { value: 3, suit: 'diamonds' },
-  ])
+  ]) === 13
 )
+
+// getHandType:: Array -> String
+const getHandType = (hand) => {
+  if (hand.some((card) => card.value === 'A')) {
+    return 'soft'
+  } else if (hand[0].value === hand[1].value) {
+    return 'pairs'
+  }
+  return 'hard'
+}
+
+console.log()
+console.log('getHandType')
+console.log(getHandType([{ value: 10 }, { value: 'A' }]) === 'soft')
+console.log(getHandType([{ value: 10 }, { value: 10 }]) === 'pairs')
+console.log(getHandType([{ value: 10 }, { value: 5 }]) === 'hard')
+
+// checkBust:: Array -> Boolean
+const checkBust = (hand) => {
+  return handTotal(hand) > 21
+}
+console.log()
+console.log('checkBust')
+console.log(checkBust([{ value: 10 }, { value: 10 }]) === false)
+console.log(checkBust([{ value: 10 }, { value: 10 }, { value: 5 }]) === true)
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import { Link } from 'react-router-dom'
+import Button from '../components/Button'
+import classNames from 'classnames'
 
 const Home = () => {
+  const number = 3
   return (
-    <main>
-      <h1>Blackjack Trainer </h1>
+    <main className="flex justify-center">
+      <h1 className={classNames('text-xl', { 'text-blue-600': number === 5 })}>Blackjack Trainer </h1>
       <Link to="/StrategyTraining">Strategy Training</Link>
+      <Button to="/" label="Click me" />
     </main>
   )
 }
