9e5c043d9978aa3b48c96f8447122f426a825267
Chris Hildebrand <hildebrandc94@gmail.com>
2023-08-15 17:47:22 -0400

    Completed buildDecks function for generating game decks

--- src/functions/pureFunctions.js
+++ src/functions/pureFunctions.js
@@ -1,4 +1,6 @@
-// hit:: -> Array -> [Object, Array]
+import { nanoid } from 'nanoid'
+
+// hit:: -> Card[] -> [Card, Card[]]
 const hit = (deckCards) => {
   const randomIndex = Math.floor(Math.random() * deckCards.length)
   const newDeck = [...deckCards.filter((_, index) => index !== randomIndex)]
@@ -8,7 +10,7 @@ const hit = (deckCards) => {
 console.log('hit')
 console.log(hit(Array.from({ length: 100 }, (_, index) => ({ id: index + 1 }))))
 
-// handTotal:: -> Array -> Number
+// handTotal:: -> Card[] -> Number
 const handTotal = (hand) => {
   return hand.reduce((acc, card) => {
     return card.value + acc
@@ -24,7 +26,7 @@ console.log(
   ]) === 13
 )
 
-// getHandType:: Array -> String
+// getHandType:: Card[] -> String
 const getHandType = (hand) => {
   if (hand.some((card) => card.value === 'A')) {
     return 'soft'
@@ -40,7 +42,7 @@ console.log(getHandType([{ value: 10 }, { value: 'A' }]) === 'soft')
 console.log(getHandType([{ value: 10 }, { value: 10 }]) === 'pairs')
 console.log(getHandType([{ value: 10 }, { value: 5 }]) === 'hard')
 
-// checkBust:: Array -> Boolean
+// checkBust:: Card[] -> Boolean
 const checkBust = (hand) => {
   return handTotal(hand) > 21
 }
@@ -48,3 +50,21 @@ console.log()
 console.log('checkBust')
 console.log(checkBust([{ value: 10 }, { value: 10 }]) === false)
 console.log(checkBust([{ value: 10 }, { value: 10 }, { value: 5 }]) === true)
+
+// buildDecks:: Number -> Card[]
+const buildDecks = (amount) => {
+  const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+  const royalCards = ['Jack', 'Queen', 'King', 'A']
+
+  const createSuitedCards = (suit) => {
+    return Array.from({ length: amount * 13 }, (_, index) => ({
+      id: nanoid(),
+      value: index <= 8 ? 2 + index : royalCards[index - 9] === 'A' ? 11 : 10,
+      name: index <= 8 ? (2 + index).toString() : royalCards[index - 9],
+      suit: suit,
+    }))
+  }
+  return suits.flatMap((suit) => createSuitedCards(suit))
+}
+
+console.log(buildDecks(1))
