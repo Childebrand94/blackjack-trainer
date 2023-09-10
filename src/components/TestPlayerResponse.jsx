12590abdd48b6fe003f6d977ed2ae9826b142ad0
Chris Hildebrand <hildebrandc94@gmail.com>
2023-09-09 20:28:34 -0400

    Give user feedback based on input

---
+++ src/components/TestPlayerResponse.jsx
@@ -0,0 +1,17 @@
+const TestPlayerResponse = ({ correctResponse, runningCount, trueCount }) => {
+  return (
+    <div className="bg-gray-400 p-4 rounded-xl absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      {correctResponse === 'correct' ? (
+        <p className="text-center text-white text-xl font-bold">Correct</p>
+      ) : (
+        <div>
+          <h2 className="text-center text-white text-2xl font-bold mb-2">Incorrect</h2>
+          <h3 className="text-center text-white text-lg">{`Running Count = ${runningCount}`}</h3>
+          <h3 className="text-center text-white text-lg">{`True Count = ${trueCount}`}</h3>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default TestPlayerResponse
