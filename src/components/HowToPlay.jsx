a8b5d18a344dd943e61e9eb2c06319451bd8c49b
Chris Hildebrand <hildebrandc94@gmail.com>
2023-09-18 16:28:00 -0400

    Created HowToPlay Component.

--- src/components/HowToPlay.jsx
+++ src/components/HowToPlay.jsx
@@ -1,17 +1,10 @@
 import { Link } from 'react-router-dom'
 
-const HowToPlay = ({ onClick }) => {
+const HowToPlay = ({ onClick, heading, instructions }) => {
   return (
     <div className="bg-gray-400 p-6 rounded-lg shadow-md text-black absolute top-1/4 left-1/4 w-3/5 text-center">
-      <h3 className="font-semibold text-2xl mx-2">Basic Strategy Practice</h3>
-      <article className="p-2">
-        In this game mode of the Blackjack trainer, you'll have the opportunity to refine your skills by following a set
-        strategy known as 'basic strategy.' Basic strategy is a proven method that, when played correctly, can
-        significantly enhance your odds of winning in Blackjack. It involves making the best possible decisions based on
-        your hand and the dealer's upcard. By practicing and mastering basic strategy, you'll improve your speed and
-        accuracy in making the right choices during the game. This not only makes you a more confident player but also
-        increases your chances of success at the Blackjack table.
-      </article>
+      <h3 className="font-semibold text-2xl mx-2">{heading}</h3>
+      <article className="p-2">{instructions}</article>
       <div className="w-full flex space-x-10 justify-center pt-4">
         <Link
           to="/StrategyCard"
@@ -23,7 +16,7 @@ const HowToPlay = ({ onClick }) => {
           onClick={onClick}
           className="px-6 ml-3 w-44 py-3 bg-gray-800 hover:bg-gray-700 text-white font-semibold rounded-full text-lg hover:text-gray-300 transition duration-300"
         >
-          Exit
+          Close
         </button>
       </div>
     </div>
---
+++ src/components/instructionalText.js
@@ -0,0 +1,35 @@
+export const instructionalText = {
+  strategy: {
+    heading: 'Basic Strategy Practice',
+    instructions: `
+        In this game mode of the Blackjack trainer, you'll have the opportunity to refine your skills by following a set
+        strategy known as 'basic strategy.' Basic strategy is a proven method that, when played correctly, can
+        significantly enhance your odds of winning in Blackjack. It involves making the best possible decisions based on
+        your hand and the dealer's upcard. By practicing and mastering basic strategy, you'll improve your speed and
+        accuracy in making the right choices during the game. This not only makes you a more confident player but also
+        increases your chances of success at the Blackjack table.  
+        `,
+  },
+  speedCounting: {
+    heading: 'High-Low Card Counting',
+    instructions: `
+    High-Low card counting in Blackjack involves assigning values to cards: +1 for low cards (2-6), -1 for high cards (10-Ace), 
+    and 0 for neutral cards (7-9). Players maintain a running count by adding and subtracting these values as cards are dealt. 
+    A higher positive count suggests more high cards remain, favoring the player. To refine their advantage, players calculate 
+    the true count by dividing the running count by the decks remaining, rounding down to the nearest whole number. A higher 
+    true count means a greater advantage. By mastering High-Low card counting and the true count, players can make better betting 
+    decisions and improve their odds of winning. 
+    `,
+  },
+
+  countingStrategy: {
+    heading: 'Realistic Scenario',
+    instructions: `In this challenging game mode, you'll be tasked with not only mastering the art of card counting but also 
+    applying flawless Blackjack strategy simultaneously. Combining both skills, you'll aim to achieve the ultimate level of mastery. 
+    The High-Low card counting system will keep you on your toes as you track the ebb and flow of the deck. Simultaneously, you'll 
+    adhere to perfect Blackjack strategy, making the most advantageous moves based on 
+    your hand and the dealer's upcard. This mode will test your ability to maintain focus, make quick calculations, and execute precise 
+    plays. It's the ultimate test of skill and strategy for those seeking to become true Blackjack masters.
+    `,
+  },
+}
